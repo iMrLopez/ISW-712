74d51de2e2ecd18aa997f78818a5ecf4c833fa1f
Marny Lopez <mlopez.cuentas@outlook.com>
2018-03-17 16:04:23 -0600

    Avance 2 (version 1.0)

---
+++ Application/ProyectoIngenieria/ProyectoIngenieria/Models/AccionAfirmativa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoIngenieria.Models
+{
+    public class AccionAfirmativa
+    {
+        public int IdAccionAfirmativa { get; set; }
+        public TipoAccionAfirmativa TipoAccionAfirmativa { get; set; }
+        public int PuntosAccionAfirmativa { get; set; }
+    }
+}
---
+++ Application/ProyectoIngenieria/ProyectoIngenieria/Models/Carrera.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public enum Carrera
+{
+    Ingeniería_en_Gestión_Ambiental,
+    Ingeniería_en_Manejo_del_Recurso_Hídrico,
+    INGENIERÍA_EN_SALUD_OCUPACIONAL_Y_AMBIENTE,
+    Ingeniería_en_Producción_Industrial,
+    Ingeniería_Electrónica,
+    Ingeniería_Electromecánica,
+    CONTROL_DE_CALIDAD_INGENIERÍA_EN_PROCESOS_Y_CALIDAD,
+    Administración_del_Comercio_Exterior,
+    Administración_y_Gestión_de_Recursos_Humanos,
+    Administración_Aduanera,
+    Compras_y_Control_de_Inventarios_Logística_Internacional,
+    Gestión_de_Empresas_de_Hospedaje_y_Gastronómicas,
+    Gestión_de_Grupos_Turísticos__Gestión_Ecoturística,
+    INGENIERÍA_DEL_SOFTWARE_TECNOLOGÍAS_INFORMÁTICAS,
+    Contabilidad_y_Finanzas_Contaduría_Pública,
+    Asistencia_Administrativa,
+    Fotografía_Tecnología_de_la_Imagen,
+    Inglés_como_Lengua_Extranjera,
+};
