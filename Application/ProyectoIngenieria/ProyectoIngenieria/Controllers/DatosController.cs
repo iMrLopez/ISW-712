3155bfa299daaa7633120a5ddb202465452d7309
Jorge <jsan18x@gmail.com>
2018-04-01 04:17:37 -0600

    Controller, model, view Aspirante Avance 2

---
+++ Application/ProyectoIngenieria/ProyectoIngenieria/Controllers/DatosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoIngenieria.Controllers
+{
+    public class DatosController : Controller
+    {
+        // GET: Datos
+        public ActionResult DatosPersonales()
+        {
+            return View();
+        }
+    }
+}
---
+++ Application/ProyectoIngenieria/ProyectoIngenieria/Models/Aspirante.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoIngenieria.Models
+{
+    public class Aspirante
+    {
+        string Cedula { get; set; }
+
+        string Nombre { get; set; }
+
+        string Apellido1 { get; set; }
+
+        string Apellido2 { get; set; }
+
+        char Sexo { get; set; }
+
+        string FechaNacimiento{ get; set; }
+
+        string Direccion { get; set; }
+
+        int Provincia { get; set; }
+
+        int Canton { get; set; }
+
+        int Distrito { get; set; }
+    }
+}
